19b7ee1d1b6fb3fed311250219f3c87afd685dc3
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-05-22 15:58:49 +0530

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="section py-10">
+    <div class="columns">
+      <div class="column px-0">
+        <div class="has-text-centered">
+          <p>&copy; Copyright 2022</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -9,4 +10,5 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {},
+  plugins: [createPersistedState()],
 });
